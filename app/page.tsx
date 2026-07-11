6cc2f4ec88f44c863995841bbe5d54928199e5e5
soran <soranlotfi2023@gmail.com>
2026-07-11 20:25:31 +0330

    initial font and styles of the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="flex gap-4 p-6 bg-red-400 shadow-xl rounded-3xl border-2 border-gold">
-        <span className="text-emerald-dark font-bold text-2xl">مبل تک</span>
+      <div className="flex bg-emerald-dark gap-4 p-6  shadow-xl rounded-3xl border-2 border-gold">
+        <span className="text-emerald-dark font-bold text-2xl bg-gold">مبل تک</span>
         <span className="text-gold text-lg">✦</span>
       </div>
     </div>
